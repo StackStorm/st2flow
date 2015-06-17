fc3eb7496e853742cd3cc933f3f95bda1455212e
Kirill Izotov <enykeev@stackstorm.com>
2015-06-17 22:59:02 +0600

    Update Intermediate#search() tests

--- tests/test_intermediate.js
+++ tests/test_intermediate.js
@@ -1,7 +1,9 @@
 'use strict';
 
-let expect = require('chai').expect
+let _ = require('lodash')
+  , expect = require('chai').expect
   , fs = require('fs')
+  , Range = require('../js/lib/range')
   ;
 
 let Intermediate = require('../js/lib/intermediate');
@@ -53,21 +55,61 @@ describe('Intermediate', () => {
       intermediate.parse(code);
     });
 
-    it('should return an array of tasks touched by the cursor', () => {
-      let coordinates = [20, 5]; // `·····|···ref: core.local`
+    it('should return an array of sectors touched by the range', () => {
+      let coordinates = new Range(16, 6, 18, 18);
+      // `······[··on-failure: setup_uninstall_pack_to_install_1
+      // `····-
+      // `········name: setu]p_uninstall_pack_to_install_1`
 
-      let result = intermediate.search(...coordinates);
+      let result = intermediate.search(coordinates);
 
-      expect(result).to.be.an('object');
-      expect(result).to.be.equal(intermediate.task('setup_uninstall_pack_to_install_1'));
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(3);
+
+      expect(result[0].type).to.be.equal('task');
+      expect(result[0].task)
+        .to.be.equal(intermediate.task('setup_check_pack_to_install_1_installed'));
+
+      expect(result[1].type).to.be.equal('task');
+      expect(result[1].task)
+        .to.be.equal(intermediate.task('setup_uninstall_pack_to_install_1'));
+
+      expect(result[2].type).to.be.equal('name');
+    });
+
+    it('should filter only specific types of sectors when requested', () => {
+      let coordinates = new Range(16, 6, 18, 18);
+      // `······[··on-failure: setup_uninstall_pack_to_install_1
+      // `····-
+      // `········name: setu]p_uninstall_pack_to_install_1`
+
+      let result = intermediate.search(coordinates, 'task');
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(2);
+
+      expect(_.every(result, 'type', 'task')).to.be.true;
     });
 
     it('should return undefined when cursor is outside all the ranges', () => {
-      let coordinates = [1, 1]; // `·|·······base_repo_url: "https://github.com/StackStorm"`
+      let coordinates = new Range(1, 1, 1, 3);
+      // `·[··]·····base_repo_url: "https://github.com/StackStorm"`
 
-      let result = intermediate.search(...coordinates);
+      let result = intermediate.search(coordinates);
 
-      expect(result).to.be.undefined;
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(0);
+    });
+
+    it('should support zero character ranges (cursor)', () => {
+      let coordinates = new Range(20, 5, 20, 5);
+      // `·····|···ref: core.local`
+
+      let result = intermediate.search(coordinates);
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0].task).to.be.equal(intermediate.task('setup_uninstall_pack_to_install_1'));
     });
 
   });
