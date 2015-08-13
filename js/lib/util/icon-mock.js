c2b21caf921c35cd512da16847d46e70ec12982a
Kirill Izotov <enykeev@stackstorm.com>
2015-08-13 13:40:08 +0600

    Remove a border from empty icons

--- js/lib/util/icon-mock.js
+++ js/lib/util/icon-mock.js
@@ -3,8 +3,11 @@ const packs = {
   linux: '/i/tux.svg'
 };
 
+const TRANSPARENT_GIF =
+  'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+
 export default function packIcon(node={}) {
   const pack = node.ref && node.ref.split('.')[0];
 
-  return pack && packs[pack] || '';
+  return pack && packs[pack] || TRANSPARENT_GIF;
 }
