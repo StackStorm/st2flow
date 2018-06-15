d39cf3e362b539a4e27dff5cbe1cc79d160d8a2c
Christopher Baker <christopher@bitovi.com>
2018-06-14 22:11:35 -0700

    add react and routes

--- main.js
+++ main.js
@@ -1,1 +1,76 @@
-import '@stackstorm/st2flow-model';
+import './style.less';
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import ReactDOM from 'react-dom';
+
+import {
+  Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import createHashHistory from 'history/createHashHistory';
+
+// import api from '@stackstorm/module-api';
+
+const history = window.routerHistory = createHashHistory({});
+
+class Editor extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      url: PropTypes.string.isRequired,
+      path: PropTypes.string.isRequired,
+      params: PropTypes.shape({
+        ref: PropTypes.string,
+      }).isRequired,
+    }).isRequired,
+  };
+
+  render() {
+    const { match: { path, params: { ref } } } = this.props;
+
+    return (
+      <div>
+        {path} {ref}
+      </div>
+    );
+  }
+}
+
+export class Container extends Component {
+  auth(bundle64) {
+    const source = JSON.parse(window.atob(bundle64));
+
+    if (source.api === undefined) {
+      source.api = `https://${window.location.hostname}:443/api`;
+      source.auth = `https://${window.location.hostname}:443/auth`;
+    }
+
+    // console.log(source);
+  }
+
+  render() {
+    return (
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={Editor} />
+          <Route path="/action/:ref" component={Editor} />
+
+          <Route
+            path="/import/:bundle/:ref?"
+            render={({ history, match: { params: { bundle, ref }} }) => {
+              this.auth(bundle);
+
+              setTimeout(() => {
+                history.push(ref ? `/action/${ref}` : '/');
+              }, 100);
+
+              return null;
+            }}
+          />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(<Container />, document.querySelector('#container'));
