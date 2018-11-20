0496e30f06c4266156bea5d9cb5bcd1763b7a646
Kirill Izotov <enykeev@stackstorm.com>
2018-11-20 17:29:53 +0700

    Save button feedback animation

--- modules/st2flow-canvas/toolbar.js
+++ modules/st2flow-canvas/toolbar.js
@@ -6,6 +6,49 @@ import cx from 'classnames';
 
 import style from './style.css';
 
+export class ToolbarButton extends Component<{
+  icon: string,
+  onClick: Function,
+}, {
+  status: 'initial' | 'pending' | 'success' | 'error',
+}> {
+  static propTypes = {
+    icon: PropTypes.string,
+    onClick: PropTypes.func,
+  }
+
+  state = {
+    status: 'initial',
+  };
+
+  async handleClick(e: Event) {
+    e.stopPropagation();
+
+    const { onClick } = this.props;
+
+    if (onClick) {
+      this.setState({ status: 'pending' });
+      try {
+        await onClick();
+        this.setState({ status: 'success' });
+      }
+      catch (e) {
+        this.setState({ status: 'error' });
+      }
+    }
+  }
+
+  style = style
+
+  render() {
+    const { icon } = this.props;
+    const { status } = this.state;
+    return (
+      <div className={cx(this.style.toolbarButton, icon, this.style[status])} onClick={e => this.handleClick(e)} />
+    );
+  }
+}
+
 export default class Toolbar extends Component<{
   children: any,
 }> {
@@ -24,7 +67,7 @@ export default class Toolbar extends Component<{
           this.props.children
             .map(button => {
               return (
-                <div key={button.key} className={cx(this.style.toolbarButton, button.props.icon)} onClick={e => button.props.onClick(e)} />
+                <ToolbarButton key={button.key} icon={button.props.icon} onClick={e => button.props.onClick(e)} />
               );
             })
         } 
