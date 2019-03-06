06773ffe4c359aeb78b968fbcae4e5f6e8a7606b
Bradley Momberger <brad@bitovi.com>
2019-03-06 15:57:20 -0500

    Remove unused code and imports from layout.js

--- modules/st2flow-model/layout.js
+++ modules/st2flow-model/layout.js
@@ -1,34 +1,10 @@
 // @flow
 
 import type { ModelInterface } from './interfaces';
-import dagre from 'dagre';
 
 export function layout(model: ModelInterface) {
-  // const g = new dagre.graphlib.Graph();
-
-  // g.setGraph({});
-
-  // g.setDefaultEdgeLabel(() => ({}));
-
   model.tasks.forEach(task => {
-    // const { size } = task;
-    // if (size) {
-    //   g.setNode(task.name, { width: size.x, height: size.y });
-    // }
     const { name } = task;
     model.updateTask({ name }, { coords: { x: -1, y: -1 }});
   });
-
-  // model.transitions.forEach(transition => {
-  //   const { from, to } = transition;
-
-  //   to.forEach(to => g.setEdge(from.name, to.name));
-  // });
-
-  // dagre.layout(g);
-
-  // g.nodes().forEach(name => {
-  //   const { x, y } = g.node(name);
-  //   model.updateTask({ name }, { coords: { x, y }});
-  // });
 }
