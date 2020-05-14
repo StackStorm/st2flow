cbff7393fdcbee9bc4f1903dfe9c5ffe00abbdb5
W Chan <m4d.coder@gmail.com>
2020-05-14 21:40:29 +0000

    Update year in the copyright headers of js files

--- modules/st2-api/tests/test-api.js
+++ modules/st2-api/tests/test-api.js
@@ -1,4 +1,4 @@
-// Copyright 2019 Extreme Networks, Inc.
+// Copyright 2020 Extreme Networks, Inc.
 //
 // Unauthorized copying of this file, via any medium is strictly
 // prohibited. Proprietary and confidential. See the LICENSE file
